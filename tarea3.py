2f9ec03e58ad083cb96e89c251d2e0e6c1074bea
AlexanderLaurente <gabriel.laurente@unmsm.edu.pe>
2024-09-21 17:03:47 -0500

    Add files via upload

---
+++ tarea3.py
@@ -0,0 +1,10 @@
+import math
+
+primera_practica = 16
+segunda_practica = 13
+tercera_practica = 19
+cuarta_practica = 8
+
+promedio_de_practicas = (primera_practica + segunda_practica + tercera_practica + cuarta_practica) / 4
+
+print("El promedio de las prácticas es:", math.floor(promedio_de_practicas))
